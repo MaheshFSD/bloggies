b032aa38b4866b0e1f3b321c8f0b8512eb494dcc
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-05-30 15:36:55 +0530

    hashing password..

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const {Schema, model} = require('mongoose');
+const { createHmac, randomBytes  } = require('node:crypto');
 
 const userSchema = new Schema({
     fullName: {
@@ -16,11 +17,33 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    profileImageUrl: {
+        type: String,
+        default: 'images/profile.png'
+    },
+    role: {
+        type: String,
+        enum: ['ADMIN',"USER","DEV"],
+        default: "USER"
     }
 },
 {
     timeStamps: true
 });
+
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    if(!user.isModified("password")) return; 
+
+    const salt = randomBytes(16).toString();
+    const hashedPwd = createHmac('sha256', salt).update(user.password).digest('hex');
+    console.log(hashedPwd, ' ------- pwd ----- ');
+    this.salt = salt;
+    this.password = hashedPwd;
+    next()
+})
 
 const User = model('User', userSchema);
 module.exports = User;
